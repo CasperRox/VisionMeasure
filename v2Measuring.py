bf79183cd9518dff73f9e08304ccaaf24bc4a8ee
Daminda <daminda.12@cse.mrt.ac.lk>
2018-01-09 13:35:14 +0530

    DayEnd

--- v2Measuring.py
+++ v2Measuring.py
@@ -1,6 +1,9 @@
 import numpy as np
 import cv2
 
+refPt = []			# to store cropping rectangle cordinates
+cropping = False	# check point for cropping
+frame = []			# to store original image frame
 
 def removeBackground1():
 	cap = cv2.VideoCapture(0)
@@ -30,8 +33,9 @@ def removeBackground2():
 	current = cv2.imread("E:\ImageProcessing\Testings\img2.jpg")
 	current = cv2.resize(current, (640, 480))
 	# current = cv2.cvtColor(current, cv2.COLOR_BGR2GRAY)
-	foreground = initial - current
-	cv2.imshow("Test", foreground)
+	# foreground = initial - current
+	# cv2.imshow("Test", foreground)
+	cv2.imshow("Test", current)
 	cv2.waitKey(10000)
 	# cap = cv2.VideoCapture(0)
 	## cap.set(cv2.CAP_PROP_SETTINGS, 1)
@@ -58,9 +62,66 @@ def removeBackground2():
 	# cv2.destroyAllWindows()
 
 
-removeBackground1()
-# removeBackground2()
+def click_and_crop(event, x, y, flags, param):
+	global refPt, cropping, frame
+	if event == cv2.EVENT_LBUTTONDOWN:			# Capturing mouse click down position
+		refPt = [(x,y)]
+		cropping = True
+	elif event == cv2.EVENT_LBUTTONUP:			# Capturing mouse click up after down position
+		refPt.append((x,y))
+		cropping = False
+		cv2.rectangle(frame, refPt[0], refPt[1], (0, 255, 0), 2)	# Drawing a rectangle on input frame selected by mouse
+		cv2.imshow("Original", frame)
 
+
+
+
+def gridMethod():
+	cap = cv2.VideoCapture(0)
+	# cap.set(cv2.CAP_PROP_SETTINGS, 1)
+	global refPt, cropping, frame
+
+	while(True):
+		# Capture frame-by-frame
+		ret, frame = cap.read()
+		if ret:
+			if (len(refPt)==2) and (refPt[1][1]-refPt[0][1]>0) and (refPt[1][0]-refPt[0][0]>0):
+				cv2.rectangle(frame, refPt[0], refPt[1], (0, 255, 0), 2)	# Drawing a rectangle on input frame selected by mouse
+				frame[0:(refPt[1][1]-refPt[0][1]), 0:(refPt[1][0]-refPt[0][0])] = frame[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]]
+			cv2.namedWindow("Original")
+
+			cv2.imshow("Original", frame)
+
+			# print (frame.shape[1])			# [height, width, noOfColors]
+			# print (frame.item(100,100,2))
+			# print frame.size()
+
+			# test = frame[350:450, 250:450]
+			# frame[200:300, 100:300] = test
+			# print (test.item(50,50,0))
+
+			clone = frame.copy()
+			# cv2.namedWindow("image")
+			cv2.setMouseCallback("Original", click_and_crop)
+			if (len(refPt)==2) and (refPt[1][1]-refPt[0][1]>0) and (refPt[1][0]-refPt[0][0]>0):
+			# if len(refPt) == 2:
+				roi = clone[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]]
+				cv2.imshow("ROI", roi)
+				frame[0:(refPt[1][1]-refPt[0][1]), 0:(refPt[1][0]-refPt[0][0])] = frame[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]]
+
+			# cv2.imshow("Processed", frame)
+
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
+
+	# When everything done, release the capture
+	cap.release()
+	cv2.destroyAllWindows()
+
+
+# removeBackground1()
+removeBackground2()
+# gridMethod()
 
 
 '''
